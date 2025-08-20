50ae253a04516bb752603d5a443b6bbecb67f4e3
tobiama <amaliagillig2508@gmail.com>
2025-08-20 09:02:37 -0300

    Delete MainThrows.java

--- MainThrows.java
+++
@@ -1,9 +0,0 @@
-package CodigoPruebaThrow;
-
-public class MainThrows {
-
-    public static void devolverTamanio() throws NullPointerException{
-        String nombre = null;
-        System.out.println("El largo del nombre es:" + nombre.length());
-    }
-}
