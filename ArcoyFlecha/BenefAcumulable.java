4f0b118d462be5752e6d72e0eeff49ca9ba9a41f
tobiama <amaliagillig2508@gmail.com>
2025-09-08 12:35:37 -0300

    Delete ArcoyFlecha directory

--- ArcoyFlecha/BenefAcumulable.java
+++
@@ -1,3 +0,0 @@
-public interface BenefAcumulable {
-    double aplicarDescuentoExtra();
-}
--- ArcoyFlecha/Beneficios.java
+++
@@ -1,5 +0,0 @@
-public interface Beneficios {
-    String getNombre();
-    String getCreador();
-    boolean isAcumulable();
-}
