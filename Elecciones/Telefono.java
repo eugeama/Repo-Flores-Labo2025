ca88aa0a700a5d3d5e8b4cd503c8cc9d2e3b5a23
tobiama <amaliagillig2508@gmail.com>
2025-09-09 19:39:06 -0300

    Update Telefono.java

--- Elecciones/Telefono.java
+++ Elecciones/Telefono.java
@@ -2,13 +2,17 @@ public abstract class Telefono extends Dispositivo implements Campania {
     private String comp_telefonica;
     private int numero;
     private String mensaje;
+    private boolean prendido;
+    private int credito;
 
     public Telefono(int nroSerie, String fabricante, String modelo, String comp_telefonica,
-                    int numero, String mensaje) {
+                    int numero, String mensaje, private boolean prendido, private int credito) {
         super(nroSerie, fabricante, modelo);
         this.comp_telefonica = comp_telefonica;
         this.numero=numero;
         this.mensaje=mensaje;
+        this.prendido=prendido;
+        this.credito= credito;
     }
     public enum COMPANIA {
         CLARO, PERSONAL, MOVISTAR, TUENTI
@@ -32,7 +36,26 @@ public abstract class Telefono extends Dispositivo implements Campania {
     public void setMensaje(String mensaje) {
         this.mensaje = mensaje;
     }
+    public boolean isPrendido() {
+        return prendido;
+    }
+
+    public void setPrendido(boolean prendido) {
+        this.prendido = prendido;
+    }
+
+    public int getCredito() {
+        return credito;
+    }
+
+    public void setCredito(int credito) {
+        this.credito = credito;
+    }
     public void enviarMensaje(String mensaje){
-            System.out.println("Conectando con la antena más cercana: Voté por el partido para un mejor futuro");
+           String respuesta= "";
+        if(prendido || credito>0){
+            respuesta= "Conectando con antena más cercana... ";
         }
+        return respuesta;
+    }
 }
