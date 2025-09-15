6f5a5dac8389338fdb1036d9345608af132624b0
tobiama <amaliagillig2508@gmail.com>
2025-09-15 10:23:23 -0300

    Update Main.java

--- tratamientoSangre/Main.java
+++ tratamientoSangre/Main.java
@@ -18,14 +18,14 @@ public class Main {
         Ninio ninio2 = new Ninio("Sofia", "Diaz", 22222, LocalDate.of(2022, 7, 25), "F", "AB-", 6, 1);
 
         Adulto adulto1 = new Adulto("Alberto", "Johnson", 33333, LocalDate.of(1985, 2, 18), "M", "O+", 38);
-        Gerente gerente1 = new Gerente("Tobías", "Flores", 44444, LocalDate.of(1945, 11, 30), "F", "B-", 78);
+        Geronte Geronte1 = new Geronte("Tobías", "Flores", 44444, LocalDate.of(1945, 11, 30), "F", "B-", 78);
 
         sistema.agregarPaciente(joven1);
         sistema.agregarPaciente(joven2);
         sistema.agregarPaciente(ninio1);
         sistema.agregarPaciente(ninio2);
         sistema.agregarPaciente(adulto1);
-        sistema.agregarPaciente(gerente1);
+        sistema.agregarPaciente(Geronte1);
 
         sistema.agregarPacienteEnTratamiento(joven1);
         sistema.agregarPacienteEnTratamiento(ninio1);
